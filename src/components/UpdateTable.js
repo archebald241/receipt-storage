487b4802b77b94f4f96f3769ee76a8e95fb3c69b
Kapitanov Oleg <archebald404@gmail.com>
2021-01-27 02:00:25 +0700

    correction of the addition form

--- src/components/UpdateTable.js
+++ src/components/UpdateTable.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Modal, Button } from 'react-bootstrap';
+import { Alert, Form, Modal, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class UpdateTable extends React.Component {
@@ -8,6 +8,7 @@ export default class UpdateTable extends React.Component {
 		super(props)
 
 		this.state = {
+			showError : false,
 			show : false,
 			datePay : '',
 			kioskName : '',
@@ -24,6 +25,7 @@ export default class UpdateTable extends React.Component {
 
 	handleClose = () => this.setState({show: false});
 	handleShow = () => this.setState({show: true});
+	errorClose = () => this.setState({showError: false});
 
 	addInTable(event){
 		const target = event.target;
@@ -46,76 +48,79 @@ export default class UpdateTable extends React.Component {
 		}	else {
 			payStatus = 'Недоплата';
 		}
-
-		var line = {
-			datePay : this.state.datePay,
-			kioskName : this.state.kioskName,
-			chequeType  : this.state.chequeType,
-			payStatus : payStatus,
-			paySum : this.state.paySum,
-			sum : this.state.sum,
-			quantityPositions : this.state.quantityPositions,
-			namePosicions : this.state.namePosicions
-		}
-		this.setState({
-			datePay : '',
-			kioskName : '',
-			chequeType : '',
-			paySum : 0,
-			sum : 0,
-			quantityPositions : 0,
-			namePosicions : ''
-		});
-		this.props.setTableText(line)
+		if (this.state.datePay != '' && this.state.kioskName != '' && this.state.namePosicions != '') {
+			var line = {
+				datePay : this.state.datePay,
+				kioskName : this.state.kioskName,
+				chequeType  : this.state.chequeType,
+				payStatus : payStatus,
+				paySum : this.state.paySum,
+				sum : this.state.sum,
+				quantityPositions : this.state.quantityPositions,
+				namePosicions : this.state.namePosicions
+			}
+			this.setState({
+				showError: false,
+				datePay : '',
+				kioskName : '',
+				chequeType : 'Продажа',
+				paySum : 0,
+				sum : 0,
+				quantityPositions : 0,
+				namePosicions : ''
+			});
+			this.props.setTableText(line)
+		} else this.setState({showError: true});
 	}
 
 	render() {
 		return (
 			<div>
 				<Button variant="primary" onClick={()=>{this.handleShow()}}>Добавить чек</Button>
-	    	<Modal className="modal-90w" show={this.state.show} onHide={()=>{this.handleClose()}}>
-		        <Modal.Header closeButton>
-			    		<Modal.Title>Добаивить чек</Modal.Title>
-		        </Modal.Header>
-		        <Modal.Body>
-							<Form>
-								<Form.Group controlId="formBasicEmail">
-									<Form.Label>Дата покупки</Form.Label>
-									<Form.Control type="datetime-local" name="datePay" onChange={this.addInTable} value={this.state.datePay} autocomplete="off" />
-								</Form.Group>
-								<Form.Group controlId="formBasicPassword">
-									<Form.Label>Киоск</Form.Label>
-									<Form.Control type="text" name="kioskName" onChange={this.addInTable} value={this.state.kioskName} autocomplete="off" />
-								</Form.Group>
-								<Form.Group controlId="formBasicPassword">
-									<Form.Label>Тип</Form.Label>
-									<Form.Control as="select" type="text" name="chequeType" onChange={this.addInTable} value={this.state.chequeType} autocomplete="off">
-										<option>Продажа</option>
-							      <option>Возврат</option>
-									</Form.Control>
-								</Form.Group>
-								<Form.Group controlId="formBasicPassword">
-									<Form.Label>Оплата</Form.Label>
-									<Form.Control type="number" min="0" name="paySum" onChange={this.addInTable} value={this.state.paySum} autocomplete="off" />
-								</Form.Group>
-								<Form.Group controlId="formBasicPassword">
-									<Form.Label>Сумма</Form.Label>
-									<Form.Control type="number" min="0" name="sum" onChange={this.addInTable} value={this.state.sum} autocomplete="off" />
-								</Form.Group>
-								<Form.Group controlId="formBasicPassword">
-									<Form.Label>Количествово товара</Form.Label>
-									<Form.Control type="number" min="1" name="quantityPositions" onChange={this.addInTable} value={this.state.quantityPositions} autocomplete="off" />
-								</Form.Group>
-								<Form.Group controlId="formBasicPassword">
-									<Form.Label>Наименование товара</Form.Label>
-									<Form.Control type="text" name="namePosicions" onChange={this.addInTable} value={this.state.namePosicions} autocomplete="off" />
-								</Form.Group>
-							</Form>
-			       </Modal.Body>
-			       <Modal.Footer>
-			       	<Button variant="secondary" onClick={()=>{this.handleClose()}}>Закрыть</Button>
-			       	<Button variant="primary" onClick={()=>{this.saveResault()}}>Сохранить</Button>
-			       </Modal.Footer>
+	    	<Modal className="modal-90w" show={this.state.show} onHide={() => {this.handleClose()}}>
+	        <Modal.Header closeButton>
+		    		<Modal.Title>Добаивить чек</Modal.Title>
+	        </Modal.Header>
+	        <Modal.Body>
+						{this.state.showError && <Alert variant={'danger'} onClose={() => {this.errorClose()}} dismissible>Введены некоректные данные</Alert>}
+						<Form>
+							<Form.Group controlId="formBasicEmail">
+								<Form.Label>Дата покупки</Form.Label>
+								<Form.Control type="datetime-local" name="datePay" onChange={this.addInTable} value={this.state.datePay} autocomplete="off" />
+							</Form.Group>
+							<Form.Group controlId="formBasicPassword">
+								<Form.Label>Киоск</Form.Label>
+								<Form.Control type="text" name="kioskName" onChange={this.addInTable} value={this.state.kioskName} autocomplete="off" />
+							</Form.Group>
+							<Form.Group controlId="formBasicPassword">
+								<Form.Label>Тип</Form.Label>
+								<Form.Control as="select" type="text" name="chequeType" onChange={this.addInTable} value={this.state.chequeType} autocomplete="off">
+									<option>Продажа</option>
+						      <option>Возврат</option>
+								</Form.Control>
+							</Form.Group>
+							<Form.Group controlId="formBasicPassword">
+								<Form.Label>Оплата</Form.Label>
+								<Form.Control type="number" min="0" name="paySum" onChange={this.addInTable} value={this.state.paySum} autocomplete="off" />
+							</Form.Group>
+							<Form.Group controlId="formBasicPassword">
+								<Form.Label>Сумма</Form.Label>
+								<Form.Control type="number" min="0" name="sum" onChange={this.addInTable} value={this.state.sum} autocomplete="off" />
+							</Form.Group>
+							<Form.Group controlId="formBasicPassword">
+								<Form.Label>Количествово товара</Form.Label>
+								<Form.Control type="number" min="1" name="quantityPositions" onChange={this.addInTable} value={this.state.quantityPositions} autocomplete="off" />
+							</Form.Group>
+							<Form.Group controlId="formBasicPassword">
+								<Form.Label>Наименование товара</Form.Label>
+								<Form.Control type="text" name="namePosicions" onChange={this.addInTable} value={this.state.namePosicions} autocomplete="off" />
+							</Form.Group>
+						</Form>
+		       </Modal.Body>
+		       <Modal.Footer>
+		       	<Button variant="secondary" onClick={()=>{this.handleClose()}}>Закрыть</Button>
+		       	<Button variant="primary" onClick={()=>{this.saveResault()}}>Сохранить</Button>
+		       </Modal.Footer>
 		    </Modal>
 	    </div>
 		);
